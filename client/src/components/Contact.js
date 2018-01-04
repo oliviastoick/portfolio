3dc571a368a8cb2aa0d43f927e3371871e281c63
Olivia Stoick <oliviastoick@gmail.com>
2018-01-04 18:35:39 -0500

    updated contact component

---
+++ client/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='container'>
+      <h3 className='header'>Contact</h3>
+      <p className='contact-img'> Olivia Stoick </p>
+      <p className='contact-aboutme'>If you are looking for product manager please feel free to reach out via e-mail at <a className='contact-link' href='mailto:oliviastoick@gmail.com'>oliviastoick@gmail.com</a>.</p>
+      <a className='contact-link' href='https://www.linkedin.com/in/olivia-stoick-5b292295/'>LinkedIn</a>
+      <a className='contact-link' href='https://github.com/oliviastoick/'>GitHub</a>
+    </div>
+  )
+}
+
+export default Contact
