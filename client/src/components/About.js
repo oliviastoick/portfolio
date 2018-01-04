97019e235d113d53bc86cec82b06ce3d2de6bf69
Olivia Stoick <oliviastoick@gmail.com>
2018-01-04 18:19:05 -0500

    updated the about component

---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container'>
+      <h3 className='about-header'>About Me</h3>
+      <p className='about'>I am a Product Manager with a passion for startup technology. I completed an intensive full stack web development course to learn the language of developers so I can communicate and manage tech teams more effectively. I am a problem solver with a foundational knowledge of web development so I can communicate and manage tech teams more effectively. </p>
+    </div>
+  )
+}
+
+export default About
