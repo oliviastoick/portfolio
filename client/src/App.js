862aae6ffe7715ae95d704754919500b0f49812c
Olivia Stoick <oliviastoick@gmail.com>
2018-01-04 18:12:37 -0500

    set up components

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Olivia Stoick</h1>
-          <h2 className='description-title'>Full Stack Web Developer</h2>
+          <h2 className='description-title'>I'm a full-stack web developer.</h2>
         </header>
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
